feb4ea1ecca7871a9bcbbe54635137eb5aa3f0b4
009Akshay <121090495+009Akshay@users.noreply.github.com>
2023-01-02 16:54:17 +0530

    Update server.js

--- 229127_Akshay/server.js
+++ 229127_Akshay/server.js
@@ -1,14 +1,14 @@
 var express=require("express");
 var app=express();
 
-var path=require("path");
-app.get('/',function(req,resp){
-    resp.sendFile(path.join(__dirname,'public/app.html'))
+// var path=require("path");
+// app.get('/',function(req,resp){
+//     resp.sendFile(path.join(__dirname,'public/app.html'))
    
-});
-app.get('/home',function(req,resp){
-    resp.sendFile(path.join(__dirname,'public/app2.html'))
-});
+// });
+// app.get('/home',function(req,resp){
+//     resp.sendFile(path.join(__dirname,'public/app2.html'))
+// });
 app.get('/about',function(req,resp){
 resp.send("<h1>hello</h1>");
     
